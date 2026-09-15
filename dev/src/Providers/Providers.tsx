24d77f715739933600018e8adbcb805f0fe7445f
Christian Garlen <chadgarval@outlook.com>
2026-09-15 19:17:41 -0400

    refactor: reorganize project structure by moving source files out of dev directory

--- dev/src/Providers/Providers.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from "../Contexts/ThemeContext";
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <ThemeProvider>
-            {children}
-        </ThemeProvider>
-    )
-}
