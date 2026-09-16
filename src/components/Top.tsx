2ed5a852321dca60f1243770cf2e347692a17708
Christian Garlen <chadgarval@outlook.com>
2026-09-16 01:38:36 -0400

    Added a link to homepage to page title

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,4 +1,5 @@
 import { ThemeButton } from "./ThemeController";
+import { Link } from "react-router-dom";
 
 interface TopProps {
     title: string;
@@ -9,7 +10,7 @@ export const Top = ({ title }: TopProps) => {
     return (
         <nav className="navbar bg-base-100 h-20 shadow-sm">
             <div className="flex justify-between w-full items-center mx-10">
-                <a className="btn btn-ghost text-xl font-bold title">{title}</a>
+                <Link to='/'><a className="btn btn-ghost text-xl font-bold title">{title}</a></Link>
                 <ThemeButton />
             </div>
         </nav>
